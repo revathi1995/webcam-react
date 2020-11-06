65ac82293998c128c7b223d2be13373478b8f866
revathi1995 <60250845+revathi1995@users.noreply.github.com>
2020-11-06 14:55:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import Webcam from "react-webcam";
 import './App.css';
 
-import { withAuthenticator } from 'aws-amplify-react'
-import { Storage } from 'aws-amplify'
+//import { withAuthenticator } from 'aws-amplify-react'
+//import { Storage } from 'aws-amplify'
 
 
 class App extends React.Component {
-    state = { fileUrl: '', file: '', filename: ''}
+   // state = { fileUrl: '', file: '', filename: ''}
       constructor(props) {
         super(props);
         this.state = {
@@ -20,24 +20,24 @@ class App extends React.Component {
         const screenshot = this.webcam.getScreenshot();
         this.setState({ screenshot });
       }
-    handleChange = e => {
-        const file = e.target.files[0]
-        this.setState({
-            fileUrl: URL.createObjectURL(file),
-            file,
-            filename: file.name
-        })
-    }
-    saveFile = () => {
-        Storage.put(this.state.filename, this.state.file)
-            .then( () => {
-                console.log('successfully saved file!')
-                this.setState({fileUrl: '', file: '', filename: ''})
-            })
-            .catch(err => {
-                console.log('error uploading file!', err)
-            })
-    }
+    //handleChange = e => {
+        //const file = e.target.files[0]
+        //this.setState({
+            //fileUrl: URL.createObjectURL(file),
+            //file,
+           // filename: file.name
+        //})
+    //}
+    //saveFile = () => {
+        //Storage.put(this.state.filename, this.state.file)
+            //.then( () => {
+            //    console.log('successfully saved file!')
+               // this.setState({fileUrl: '', file: '', filename: ''})
+            //})
+            //.catch(err => {
+               // console.log('error uploading file!', err)
+            //})
+    //}
       render() {
         return (
           <div>
@@ -53,14 +53,14 @@ class App extends React.Component {
                 <div className='controls'>
                   <input type='file' onChange={this.handleChange} />
                   <button onClick={this.handleClick}>capture</button>
-                  <button onClick={this.saveFile}>Save File</button>
+                  //<button onClick={this.saveFile}>Save File</button>
                 </div>
                  <img src={this.state.screenshot} />
-                 <img src={this.state.fileUrl} />
+                 //<img src={this.state.fileUrl} />
               </div>
             </div>
           </div>
         );
       }
     }
-export default withAuthenticator(App,{includeGreetings: true});
+export default App;
