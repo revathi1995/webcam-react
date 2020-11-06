5f16c99380605e857e716fce7c9ca8a462d59022
revathi1995 <60250845+revathi1995@users.noreply.github.com>
2020-11-06 14:49:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from "react";
 import Webcam from "react-webcam";
 import './App.css';
 
+import { withAuthenticator } from 'aws-amplify-react'
+import { Storage } from 'aws-amplify'
+
+
 class App extends React.Component {
+    state = { fileUrl: '', file: '', filename: ''}
       constructor(props) {
         super(props);
         this.state = {
@@ -15,7 +20,24 @@ class App extends React.Component {
         const screenshot = this.webcam.getScreenshot();
         this.setState({ screenshot });
       }
-
+    handleChange = e => {
+        const file = e.target.files[0]
+        this.setState({
+            fileUrl: URL.createObjectURL(file),
+            file,
+            filename: file.name
+        })
+    }
+    saveFile = () => {
+        Storage.put(this.state.filename, this.state.file)
+            .then( () => {
+                console.log('successfully saved file!')
+                this.setState({fileUrl: '', file: '', filename: ''})
+            })
+            .catch(err => {
+                console.log('error uploading file!', err)
+            })
+    }
       render() {
         return (
           <div>
@@ -29,13 +51,16 @@ class App extends React.Component {
               <h2>Screenshots</h2>
               <div className='screenshots'>
                 <div className='controls'>
+                  <input type='file' onChange={this.handleChange} />
                   <button onClick={this.handleClick}>capture</button>
+                  <button onClick={this.saveFile}>Save File</button>
                 </div>
-                {this.state.screenshot ? <img src={this.state.screenshot} /> : null}
+                 <img src={this.state.screenshot} />
+                 <img src={this.state.fileUrl} />
               </div>
             </div>
           </div>
         );
       }
     }
-export default App;
+export default withAuthenticator(App,{includeGreetings: true});
