f467223b463ceec73789297fcf6f21004de0f17a
revathi1995 <60250845+revathi1995@users.noreply.github.com>
2020-11-06 15:04:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import React from "react";
 import Webcam from "react-webcam";
 import './App.css';
 
-//import { withAuthenticator } from 'aws-amplify-react'
-//import { Storage } from 'aws-amplify'
-
-
 class App extends React.Component {
-   // state = { fileUrl: '', file: '', filename: ''}
+  
       constructor(props) {
         super(props);
         this.state = {
@@ -20,24 +16,7 @@ class App extends React.Component {
         const screenshot = this.webcam.getScreenshot();
         this.setState({ screenshot });
       }
-    //handleChange = e => {
-        //const file = e.target.files[0]
-        //this.setState({
-            //fileUrl: URL.createObjectURL(file),
-            //file,
-           // filename: file.name
-        //})
-    //}
-    //saveFile = () => {
-        //Storage.put(this.state.filename, this.state.file)
-            //.then( () => {
-            //    console.log('successfully saved file!')
-               // this.setState({fileUrl: '', file: '', filename: ''})
-            //})
-            //.catch(err => {
-               // console.log('error uploading file!', err)
-            //})
-    //}
+    
       render() {
         return (
           <div>
@@ -51,12 +30,12 @@ class App extends React.Component {
               <h2>Screenshots</h2>
               <div className='screenshots'>
                 <div className='controls'>
-                  //<input type='file' onChange={this.handleChange} />
+                  
                   <button onClick={this.handleClick}>capture</button>
-                  //<button onClick={this.saveFile}>Save File</button>
+                  
                 </div>
                  <img src={this.state.screenshot} />
-                 //<img src={this.state.fileUrl} />
+                 
               </div>
             </div>
           </div>
